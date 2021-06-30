dcc5228f6107832f565bf282340a30f7ed624b14
tinoweb <tino477@gmail.com>
2021-06-29 21:32:05 -0300

    finalizacao da extrutura laravel e vue -- login cadastro funcional

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,54 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+import VueRouter from 'vue-router'
+import routes from './rotas'
+
+Vue.use(VueRouter)
+const router = new VueRouter(routes)
+
+import Toaster from 'v-toaster'
+import 'v-toaster/dist/v-toaster.css'
+Vue.use(Toaster, {timeout: 5000})
+
+import Swal from 'sweetalert2'
+window.Swal = Swal
+
+
+function loggedIn(){
+    return localStorage.getItem('token')
+    console.log('nao logado')
+}
+
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // Esta routa requer auth, checa se esta logado
+        // Se nao redireciona para a pagina de login
+        if (!loggedIn()) {
+            next({
+            path: '/login',
+            query: { redirect: to.fullPath }
+            })
+        } else {
+            next()
+        }
+    } else if(to.matched.some(record => record.meta.guest)) {
+        if (loggedIn()) {
+            next({
+            path: '/',
+            query: { redirect: to.fullPath }
+            })
+        } else {
+            next()
+        }
+    } else {
+        next() // tenha certeza que sempre deve chamar next()!
+    }
+})
+
+
+const app = new Vue({
+    el: '#app',
+    router
+});
